b8e1799fc9171337b43237c6dcda892578050b11
muhammadfajarr30 <muhammad.fajarr362@gmail.com>
2026-05-21 11:19:29 +0700

    feat:add kelvin temperature conversion

--- utils/kelvin.js
+++ utils/kelvin.js
@@ -1,8 +1,17 @@
 // Implement functions here 👇
 
-// TODO: Uncomment after implemented
+function kelvinToCelcius(value) {
+  return value + 273.15;
+}
+function kelvinToRankie(value) {
+  return (value + 273.15) * 1.8;
+}
+function kelvintoFahrenheit(value) {
+  return (value * 9) / 5 + 32;
+}
+
 module.exports = {
-  // kelvinToCelsius,
-  // kelvinToRankine,
-  // kelvinToFahrenheit,
+  kelvinToCelcius,
+  kelvinToRankine,
+  kelvinToFahrenheit,
 };
