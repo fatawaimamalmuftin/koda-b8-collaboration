43699142888243d70efa809dd5a83b64a8a990b2
Muhammadwahyu1212 <muhammad123wahyu123@gmail.com>
2026-05-21 11:20:59 +0700

    feat: add feat fuction throw error in index.js and ./utils/celsius.js

--- index.js
+++ index.js
@@ -1,30 +1,35 @@
 // TODO: Uncomment after implemented
 const {
-  celsiusToKelvin,
-  celsiusToRankine,
-  celsiusToFahrenheit,
+    celsiusToKelvin,
+    celsiusToRankine,
+    celsiusToFahrenheit,
 } = require("./utils/celsius.js");
 
 const {
-  // kelvinToCelsius,
-  // kelvinToRankine,
-  // kelvinToFahrenheit,
+    // kelvinToCelsius,
+    // kelvinToRankine,
+    // kelvinToFahrenheit,
 } = require("./utils/kelvin.js");
 
 const {
-  // rankineToCelsius,
-  // rankineToKelvin,
-  // rankineToFahrenheit,
+    // rankineToCelsius,
+    // rankineToKelvin,
+    // rankineToFahrenheit,
 } = require("./utils/rankine.js");
 
 const {
-  // fahrenheitToCelsius,
-  // fahrenheitToKelvin,
-  // fahrenheitToRankine,
+    // fahrenheitToCelsius,
+    fahrenheitToKelvin,
+    // fahrenheitToRankine,
 } = require("./utils/fahrenheit.js");
 
 const inputSuhu = 100;
+try {
+    console.log("Celcius ke fahrenheit : " + celsiusToFahrenheit(inputSuhu));
+    console.log("Celcius ke Kelvin     : " + celsiusToKelvin(inputSuhu));
+    console.log("Celcius ke Rankine    : " + celsiusToRankine(inputSuhu));
+} catch (err) {
+    console.log("Error :", err)
+}
 
-console.log("Celcius ke fahrenheit : " + celsiusToFahrenheit(inputSuhu));
-console.log("Celcius ke Kelvin     : " + celsiusToKelvin(inputSuhu));
-console.log("Celcius ke Rankine    : " + celsiusToRankine(inputSuhu));
+
